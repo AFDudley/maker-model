65a0e8706f7e07dd52cf5e20a425f212667f0d8b
Nikolai Mushegian <nikolai.mushegian@gmail.com>
2015-12-03 21:06:06 -0500

    rename

--- test_cdp.py
+++ test_cdp.py
@@ -1,11 +1,11 @@
 import unittest
-import CDPEngine
+import cdp
 
 class TestBasicCDP(unittest.TestCase):
     def setUp(self):
-        cdp1 = CDPEngine.CDP(self.params, self.feeds, self.env)
+        cdp1 = cdp.CDP(self.params, self.feeds, self.env)
         self.env.actor = 'bob'
-        cdp2 = CDPEngine.CDP(self.params, self.feeds, self.env)
+        cdp2 = cdp.CDP(self.params, self.feeds, self.env)
         self.env.actor = 'alice' # reset to alice
 
     def test_issue_just_enough(self):
